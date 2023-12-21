95135c0e4de977702473fed30d70679a84efeb1e
Albert de Weerd <70281601+NGLN@users.noreply.github.com>
2023-12-21 22:45:27 +0100

    Finaly part II
    With floodfill on an expanded map.

--- Day10 - Pipe Maze/AOC2023_10.dpr
+++ Day10 - Pipe Maze/AOC2023_10.dpr
@@ -9,7 +9,8 @@ uses
   System.Types,
   System.Generics.Collections,
   System.Generics.Defaults,
-  System.Math;
+  System.Math,
+  Vcl.Graphics;
 
 type
   TCoord = record
@@ -32,6 +33,9 @@ begin
   Result := (ACoord.X = X) and (ACoord.Y = Y);
 end;
 
+type
+  TGrid = array of array of Char;
+
 var
   Input: TStringList;
   Prev1: TCoord;
@@ -41,34 +45,37 @@ var
   Next1: TCoord;
   Next2: TCoord;
   StepCount: Integer;
+  Grid1: TGrid;
+  Grid2: TGrid;
+  Grid: TGrid;
+  Bmp: TBitmap;
+  X: Integer;
+  Y: Integer;
+  ColCount: Integer;
+  RowCount: Integer;
 
 function FindStart: TCoord;
 var
   X: Integer;
   Y: Integer;
 begin
-  for Y := 0 to Input.Count - 1 do
-    for X := 1 to Length(Input[0]) do
-      if Input[Y][X] = 'S' then
+  for Y := 0 to RowCount - 1 do
+    for X := 0 to ColCount - 1 do
+      if Grid[X, Y] = 'S' then
       begin
         Result := TCoord.Create(X, Y);
         Exit;
-      end;
+      end
 end;
 
-function CoordChar(ACoord: TCoord): Char;
+function NextMove(const AFrom, AVia: TCoord; LeaveTrace: Boolean): TCoord;
 begin
-  Result := Input[ACoord.Y][ACoord.X];
-end;
-
-function NextMove(const AFrom, AVia: TCoord): TCoord;
-begin
-  case CoordChar(AVia) of
-    '|': if AFrom.Y < AVia.Y then
+  case Grid[AVia.X, AVia.Y] of
+    '|', 'V': if AFrom.Y < AVia.Y then
            Result := TCoord.Create(AVia.X, AVia.Y + 1)
          else
            Result := TCoord.Create(AVia.X, AVia.Y - 1);
-    '-': if AFrom.X < AVia.X then
+    '-', 'H': if AFrom.X < AVia.X then
            Result := TCoord.Create(AVia.X + 1, AVia.Y)
          else
            Result := TCoord.Create(AVia.X - 1, AVia.Y);
@@ -88,36 +95,89 @@ begin
            Result := TCoord.Create(AVia.X + 1, AVia.Y)
          else
            Result := TCoord.Create(AVia.X, AVia.Y + 1);
-    else
-      WriteLn('False');
   end;
+  if LeaveTrace then
+    Grid[AVia.X, AVia.Y] := 'S';
+end;
+
+procedure WalkPath(LeaveTrace: Boolean);
+begin
+  Prev1 := FindStart;
+  Prev2 := Prev1;
+  Current1 := TCoord.Create(Prev1.X - 1, Prev1.Y);
+  Current2 := TCoord.Create(Prev2.X, Prev2.Y + 1);
+  StepCount := 1;
+  while not Current1.Equals(Current2) do
+  begin
+    Next1 := NextMove(Prev1, Current1, LeaveTrace);
+    Next2 := NextMove(Prev2, Current2, LeaveTrace);
+    Prev1 := Current1;
+    Prev2 := Current2;
+    Current1 := Next1;
+    Current2 := Next2;
+    Inc(StepCount);
+  end;
+  if LeaveTrace then
+    Grid[Current1.X, Current1.Y] := 'S';
 end;
 
 begin
   Input := TStringList.Create;
+  Bmp := TBitmap.Create;
   try
     Input.LoadFromFile('input.txt');
   { Part I }
-    Prev1 := FindStart;
-    Prev2 := Prev1;
-    Current1 := TCoord.Create(Prev1.X - 1, Prev1.Y);
-    Current2 := TCoord.Create(Prev2.X, Prev2.Y + 1);
-    StepCount := 1;
-    while not Current1.Equals(Current2) do
-    begin 
-      Next1 := NextMove(Prev1, Current1);
-      Next2 := NextMove(Prev2, Current2);
-      Prev1 := Current1;
-      Prev2 := Current2;
-      Current1 := Next1;
-      Current2 := Next2;
-      Inc(StepCount);
-    end;
+    ColCount := Length(Input[0]);
+    RowCount := Input.Count;
+    SetLength(Grid1, ColCount, RowCount);
+    Grid := Grid1;
+    for X := 0 to ColCount - 1 do
+      for Y := 0 to RowCount - 1 do
+        Grid[X, Y] := Input[Y][X + 1];
+    WalkPath(False);
     WriteLn('Part I: ', StepCount);
   { Part II }
-  
-    WriteLn('Part II: ');
+    ColCount := 2 * ColCount + 1;
+    RowCount := 2 * RowCount + 1;
+    SetLength(Grid2, ColCount, RowCount);
+    Grid := Grid2;
+    for X := 0 to ColCount - 1 do
+      for Y := 0 to RowCount - 1 do
+        if (X mod 2 = 1) and (Y mod 2 = 1) then
+          Grid[X, Y] := Grid1[X div 2, Y div 2]
+        else
+          Grid[X, Y] := ' ';
+    for X := 0 to ColCount - 1 do
+      for Y := 0 to RowCount - 1 do
+        if (X > 0) and (X < ColCount - 1) and
+          CharInSet(Grid[X - 1, Y], ['F', '-', 'L', 'S']) and
+          CharInSet(Grid[X + 1, Y], ['7', '-', 'J', 'S']) then
+            Grid[X, Y] := 'H'
+        else if (Y > 0) and (Y < RowCount - 1) and
+          CharInSet(Grid[X, Y - 1], ['F', '|', '7', 'S']) and
+          CharInSet(Grid[X, Y + 1], ['L', '|', 'J', 'S']) then
+            Grid[X, Y] := 'V';
+    WalkPath(True);
+    Bmp.SetSize(ColCount, RowCount);
+    for X := 0 to ColCount - 1 do
+      for Y := 0 to RowCount - 1 do
+        if Grid[X, Y] = 'S' then
+          Bmp.Canvas.Pixels[X, Y] := clGreen
+        else if CharInSet(Grid[X, Y], ['|', '-', 'F', 'J', '7', 'L', '.']) then
+          Bmp.Canvas.Pixels[X, Y] := clBlue
+        else
+          Bmp.Canvas.Pixels[X, Y] := clRed;
+    Bmp.Canvas.Brush.Color := clGreen;
+    Bmp.Canvas.Brush.Style := bsSolid;
+    Bmp.Canvas.FloodFill(0, 0, clGreen, fsBorder);
+    StepCount := 0;
+    for X := 0 to ColCount - 1 do
+      for Y := 0 to RowCount - 1 do
+        if Bmp.Canvas.Pixels[X, Y] = clBlue then
+          Inc(StepCount);
+    WriteLn('Part II: ', StepCount);
   finally
+    Bmp.Free;
     Input.Free;
   end;
   ReadLn;
