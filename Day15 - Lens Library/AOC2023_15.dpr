3acd83db1042141c7d46a3e7bdac4a0ba519e4ed
Albert de Weerd <70281601+NGLN@users.noreply.github.com>
2024-01-08 16:48:06 +0100

    Day 15 - Part II

--- Day15 - Lens Library/AOC2023_15.dpr
+++ Day15 - Lens Library/AOC2023_15.dpr
@@ -4,13 +4,16 @@ program AOC2023_15;
 
 uses
   System.Classes,
-  System.SysUtils;
+  System.SysUtils,
+  System.Generics.Collections;
 
 var
   Input: TStringList;
   InitSeq: TArray<String>;
   S: String;
   SumOfHashes: Integer = 0;
+  Boxes: TObjectList<TStringList>;
+  I: Integer;
 
 function Hash(const S: String): Integer;
 var
@@ -25,8 +28,48 @@ begin
   end;
 end;
 
+procedure Process(const S: String);
+var
+  Lable: String;
+  BoxId: Integer;
+  FocalStrength: Integer;
+  LensId: Integer;
+begin
+  if S[Length(S)] = '-' then
+  begin
+    Lable := Copy(S, 1, Length(S) - 1);
+    BoxId := Hash(Lable);
+    LensId := Boxes[BoxId].IndexOf(Lable);
+    if LensId > -1 then
+      Boxes[BoxId].Delete(LensId);
+  end
+  else
+  begin
+    Lable := Copy(S, 1, Length(S) - 2);
+    BoxId := Hash(Lable);
+    LensId := Boxes[BoxId].IndexOf(Lable);
+    FocalStrength := StrToInt(S[Length(S)]);
+    if LensId > -1 then
+      Boxes[BoxId].Objects[LensId] := TObject(FocalStrength)
+    else
+      Boxes[BoxId].AddObject(Lable, TObject(FocalStrength));
+  end;
+end;
+
+function FocusingPower: Integer;
+var
+  I: Integer;
+  J: Integer;
+begin
+  Result := 0;
+  for I := 0 to Boxes.Count - 1 do
+    for J := 0 to Boxes[I].Count - 1 do
+      Inc(Result, (I + 1) * (J + 1) * Integer(Boxes[I].Objects[J]));
+end;
+
 begin
   Input := TStringList.Create;
+  Boxes := TObjectList<TStringList>.Create(True);
   try
     Input.LoadFromFile('input.txt');
   { Part I }
@@ -35,9 +78,13 @@ begin
       Inc(SumOfHashes, Hash(S));
     WriteLn('Part I: ', SumOfHashes);
   { Part II }
-
-    WriteLn('Part II: ');
+    for I := 0 to 255 do
+      Boxes.Add(TStringList.Create);
+    for S in InitSeq do
+      Process(S);
+    WriteLn('Part II: ', FocusingPower);
   finally
+    Boxes.Free;
     Input.Free;
   end;
   ReadLn;
