1bbef5782f443ec8b2cff79af320cb94fdf3114d
Albert de Weerd <70281601+NGLN@users.noreply.github.com>
2024-01-15 12:27:50 +0100

    Day 18 - Part II, wrong solution !?!

--- Day18 - Lavaduct Lagoon/AOC2023_18.dpr
+++ Day18 - Lavaduct Lagoon/AOC2023_18.dpr
@@ -6,125 +6,105 @@ uses
   System.Classes,
   System.SysUtils,
   Aw.Types,
-  Vcl.Graphics,
-  System.Math;
+  System.Math,
+  System.Generics.Collections;
 
 var
   Input: TStringList;
-  Grid: array of array of Char;
-  X: Integer;
-  Y: Integer;
-  ColCount: Integer;
-  RowCount: Integer;
-  Curr: TCoord;
-  Dir: Char;
+  Coords: TList<TCoord>;
   DigCount: Integer = 0;
-  TrenchLength: Integer = 0;
-  Line: String;
-  Bmp: TBitmap;
-  Volume: Integer = 0;
 
-function InGrid(const ACoord: TCoord): Boolean;
-begin
-  Result := (ACoord.X >= 0) and (ACoord.X < ColCount) and
-    (ACoord.Y >= 0) and (ACoord.Y < RowCount);
-end;
-
-function Next(const ACoord: TCoord; ADir: Char): TCoord;
-begin
-  Result := ACoord;
-  case ADir of
-    'U': Dec(Result.Y);
-    'R': Inc(Result.X);
-    'D': Inc(Result.Y);
-    'L': Dec(Result.X);
-  end;
-end;
-
-procedure Process(const S: String);
+procedure ReadCoords(PartII: Boolean);
 var
-  A: TArray<String>;
-  Count: Integer;
-  I: Integer;
-begin
-  A := S.Split([' ']);
-  Dir := A[0][1];
-  Count := A[1].ToInteger;
-  for I := 0 to Count - 1 do
-  begin
-    Curr := Next(Curr, Dir);
-    Grid[Curr.X, Curr.Y] := '#';
-  end;
-end;
-
-procedure Init;
-var
+  Current: TCoord;
   Line: String;
-  A: TArray<String>;
+  Args: TArray<String>;
   Count: Integer;
-  MinX: Integer;
-  MinY: Integer;
-  MaxX: Integer;
-  MaxY: Integer;
+  Dir: Char;
 begin
-  MinX := 0;
-  MinY := 0;
-  MaxX := 0;
-  MaxY := 0;
-  Curr := Coord(0, 0);
+  Coords.Clear;
+  Current := Coord(0, 0);
+  Coords.Add(Current);
   for Line in Input do
   begin
-    A := Line.Split([' ']);
-    Dir := A[0][1];
-    Count := A[1].ToInteger;
+    Args := Line.Split([' ']);
+    if PartII then
+    begin
+      Dir := Args[2][8];
+      Count := StrToInt('$' + Copy(Args[2], 3, 5));
+    end
+    else
+    begin
+      Dir := Args[0][1];
+      Count := Args[1].ToInteger;
+    end;
     Inc(DigCount, Count);
     case Dir of
-      'U': Dec(Curr.Y, Count);
-      'R': Inc(Curr.X, Count);
-      'D': Inc(Curr.Y, Count);
-      'L': Dec(Curr.X, Count);
+      'R', '0': Inc(Current.X, Count);
+      'D', '1': Inc(Current.Y, Count);
+      'L', '2': Dec(Current.X, Count);
+      'U', '3': Dec(Current.Y, Count);
     end;
-    MinX := Min(MinX, Curr.X);
-    MinY := Min(MinY, Curr.Y);
-    MaxX := Max(MaxX, Curr.X);
-    MaxY := Max(MaxY, Curr.Y);
+    Coords.Add(Current);
   end;
-  ColCount := MaxX - MinX + 3;
-  RowCount := MaxY - MinY + 3;
-  SetLength(Grid, ColCount, RowCount);
-  Curr := Coord(-MinX + 1, -MinY + 1);
+end;
+
+function Volume: Int64;
+var
+  I: Integer;
+begin
+  Result := 0;
+  for I := 0 to Coords.Count - 2 do
+  begin
+    Inc(Result, Coords[I].X * Coords[I + 1].Y);
+    Dec(Result, Coords[I + 1].X * Coords[I].Y);
+  end;
+  Result := Result div 2;
+  Inc(Result, DigCount div 2 + 1);
+end;
+
+function Volume2: Int64;
+var
+  I: Integer;
+begin
+  Result := 0;
+  for I := 0 to Coords.Count - 1 do
+  begin
+    if I = 0 then
+      Inc(Result, Coords[I].X * (Coords[I + 1].Y - Coords.Last.Y))
+    else if I = Coords.Count - 1 then
+      Inc(Result, Coords[I].X * (Coords.First.Y - Coords[I - 1].Y))
+    else
+      Inc(Result, Coords[I].X * (Coords[I + 1].Y - Coords[I - 1].Y));
+  end;
+  Result := Result div 2;
+  Inc(Result, DigCount div 2 + 1);
 end;
 
 begin
   Input := TStringList.Create;
-  Bmp := TBitmap.Create;
+  Coords := TList<TCoord>.Create;
   try
     Input.LoadFromFile('input.txt');
+//    Input.LoadFromFile('example.txt');
   { Part I }
-    Init;
-    for Line in Input do
-      Process(Line);
-    Bmp.SetSize(ColCount, RowCount);
-    for X := 0 to ColCount - 1 do
-      for Y := 0 to RowCount - 1 do
-        if Grid[X, Y] = '#' then
-          Bmp.Canvas.Pixels[X, Y] := clGreen
-        else
-          Bmp.Canvas.Pixels[X, Y] := clBlue;
-    Bmp.Canvas.Brush.Color := clRed;
-    Bmp.Canvas.Brush.Style := bsSolid;
-    Bmp.Canvas.FloodFill(0, 0, clGreen, fsBorder);
-    for X := 0 to ColCount - 1 do
-      for Y := 0 to RowCount - 1 do
-        if Bmp.Canvas.Pixels[X, Y] <> clRed then
-          Inc(Volume);
+    ReadCoords(False);
+    WriteLn('DigCount = ', DigCount);
     WriteLn('Part I: ', Volume);
   { Part II }
+    ReadCoords(True);
+    WriteLn('DigCount = ', DigCount);
+    WriteLn('Part II: ', Volume);
+    WriteLn('Part II: ', Volume2);
 
-    WriteLn('Part II: ');
+    // BUT 177243763228642 is too high ?!?!?!?
+
   finally
-    Bmp.Free;
+    Coords.Free;
     Input.Free;
   end;
   ReadLn;
 end.
+
+
+
